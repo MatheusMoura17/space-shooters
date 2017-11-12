7b6401b81f9215f705ff791aca6a712999548191
Rafael Silva <rafa.dev@hotmail.com>
2017-11-11 23:31:20 -0200

    Main Menu
    Atualização da estrutura Facade ( Matheus precisa conferir se esta certo )

---
+++ Assets/Scripts/Controllers/MainMenuCtrl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCtrl : MonoBehaviour {
+
+    HudFacade hud = new HudFacade("MainMenu");
+
+    public void ChangeScene(string nameNextScene)
+    {
+        hud.SetNextScene(nameNextScene);
+    }
+}
---
+++ Assets/Scripts/Facade/HudFacade.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HudFacade
+{
+    ChangeScene _changeScene;
+
+    public HudFacade(string nameMenu)
+    {
+        _changeScene = new ChangeScene();
+    }
+  
+    public void SetNextScene(string nameNextScene)
+    {
+        _changeScene.SetNextScene(nameNextScene);
+    }
+}
