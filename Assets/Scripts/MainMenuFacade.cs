422f824ed16fbb00b82e0a8a4f4ef08e44eae276
Rafael Silva <rafa.dev@hotmail.com>
2017-11-15 14:24:08 -0200

    alterações na classe facade, 1 aquivo com as classes

---
+++ Assets/Scripts/MainMenuFacade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFacade : MonoBehaviour {
+
+    ChangeScene _changeScene;
+
+    public void ChangeScene(string nameNextScene)
+    {
+        _changeScene.SetNextScene(nameNextScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    
+}
+
+public class ChangeScene
+{
+    public void SetNextScene(string nameNextScene)
+    {
+        SceneManager.LoadScene(nameNextScene);
+    }
+}
+
+
+
