b940eda08c5b1d47417bfa74b421ab4054a54456
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-21 09:00:27 -0300

    feature: adcionando o codigo ao script do fire controller

---
+++ Assets/scripts/firecontroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class firecontroller : MonoBehaviour
+{
+    public GameObject shot1;
+    public Transform shotSpawn1;
+    public GameObject shot2;
+    public Transform shotSpawn2;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot1, shotSpawn1.position, shotSpawn2.rotation);
+            Instantiate(shot2, shotSpawn1.position, shotSpawn2.rotation);
+        }
+    }
+}
