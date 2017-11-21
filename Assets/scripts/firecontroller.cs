98a8821016bfad58b98ad1f573af18ada5ccd92e
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-21 10:11:10 -0300

    Refactor: alterando o codigo do script firecontroller

--- Assets/scripts/firecontroller.cs
+++ Assets/scripts/firecontroller.cs
@@ -3,21 +3,54 @@ using System.Collections;
 
 public class firecontroller : MonoBehaviour
 {
-    public GameObject shot1;
-    public Transform shotSpawn1;
-    public GameObject shot2;
-    public Transform shotSpawn2;
-    public float fireRate;
+    public float fireDamage = 5;
+    public float fireRate = .25f;
+    public float weapoRange = 50;
+    public float hitForce = 10;
 
+    public Transform spawnfire1;
+    public Transform spawnfire2;
+
+    private WaitForSeconds durationFire = new WaitForSeconds(5f);
+    private AudioSource fireAudio;
+    private LineRenderer laserLine;
     private float nextFire;
+    private Camera naveCamera;
+
+    void Start() {
+
+        laserLine = GetComponent<LineRenderer>();
+        fireAudio = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        if(Input.GetButtonDown("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
-            Instantiate(shot1, shotSpawn1.position, shotSpawn2.rotation);
-            Instantiate(shot2, shotSpawn1.position, shotSpawn2.rotation);
+            StartCoroutine(FireEffects());
+
+            Vector3 rayOrigin = naveCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            laserLine.SetPosition(0, spawnfire1.position);
+            laserLine.SetPosition(0, spawnfire2.position);
+
+            if(Physics.Raycast(rayOrigin, naveCamera.transform.forward, out hit, weapoRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (naveCamera.transform.forward * weapoRange));
+            }
         }
+    }
+
+    private IEnumerator FireEffects()
+    {
+        fireAudio.Play();
+        laserLine.enabled = true;
+        yield return durationFire;
+        laserLine.enabled = false;
     }
 }
---
+++ Assets/scripts/velofire.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class velofire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject nave01;
+        nave01 = GameObject.Find("default");
+
+        GetComponent<Rigidbody>().AddForce(nave01.transform.position * 100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
