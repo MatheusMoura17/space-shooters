165ca98b637775c62f7ca184447e0bf15b1e95fc
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-23 10:51:44 -0300

    feat: Classe fachada criada para o Login

---
+++ Assets/scripts/LoginFacade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginFacade : MonoBehaviour {
+
+	public InputField userNameInput;
+
+    public void ChangeScene(string nameNextScene)
+    {
+		SceneManager.LoadScene(nameNextScene);
+    }
+
+	public void Login()
+	{
+		if (userNameInput.text != "" && userNameInput.text!=" ") {
+			//checar login
+		}
+	}
+}
+
+
