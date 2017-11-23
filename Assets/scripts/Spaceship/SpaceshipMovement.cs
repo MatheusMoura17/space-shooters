9eed4269ceff3ba29a391e8b9acaa28bc72ce6fc
Matheus Moura <matheus.moura@prodeaf.net>
2017-11-23 09:31:17 -0300

    feat: Conteúdo produzido por marlon adicionado

---
+++ Assets/scripts/Spaceship/SpaceshipMovement.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour {
+
+	public bool isLocal = true;
+
+	public float acceleration = 0.8f;		// Aumente este número para a nave chegar mais rápido na vel. máxima
+	public float maxSpeed = 2.0f;			// Aumente este número para a nave ter uma velociade maior
+	public float power = 0.0f;				// Tente não mudar isso aqui
+
+	float amountRotation = 2.0f;			// Incremento da rotação da nave
+	float verticalSpeed = 0.0f;				// Velocidade de rotação vertical da nave
+	float horizontalSpeed = 0.0f;			// Velocidade de rotação horizontal da nave
+	float vertRotationLimit = 1.5f;			// Limite da velocidade de rotação vertical da nave
+	float horRotationLimit = 1.0f;			// Limite de velocidade de rotação horizontal da nave
+
+	public static Transform transformSpaceship;
+
+	void Start () {
+		transformSpaceship = transform;
+	}
+
+	void LateUpdate () {
+
+		if (isLocal) {		// Matheus pediu pra colocar
+
+			// Aceleração e desaceleração da nave
+			if (Input.GetKey ("space")) {		// Ao segurar espaço, a velocidade atual (power) irá aumentar gradativamente
+
+				if (power < maxSpeed) {
+					power += Time.deltaTime * acceleration;
+				} else {
+					power = maxSpeed;
+				}
+
+				transform.Translate (0, 0, power);
+
+			} else {							// Ao não segurar espaço, a nave irá desacelerar
+		
+				if (power > 0.0f) {				
+					power -= Time.deltaTime * acceleration;
+					transform.Translate (0, 0, power);
+
+				} else {				
+					power = 0.0f;			
+				}
+			}
+
+
+			// Altitude
+			// Subindo
+			if (Input.GetKey ("down")) {
+
+				if (verticalSpeed > -vertRotationLimit) {
+					verticalSpeed -= amountRotation * Time.deltaTime;
+				} else {
+					verticalSpeed = -vertRotationLimit;
+				}
+
+				transform.Rotate (verticalSpeed, 0, 0);
+
+			}
+
+
+			// Descendo
+			if (Input.GetKey ("up")) {
+
+				if (verticalSpeed < vertRotationLimit) {
+					verticalSpeed += amountRotation * Time.deltaTime;
+				} else {
+					verticalSpeed = vertRotationLimit;
+				}
+
+				transform.Rotate (verticalSpeed, 0, 0);
+			}
+
+
+			// Resetando a velocidade de rotação vertical
+			if (Input.GetKeyUp ("up") || Input.GetKeyUp ("down")) {
+				verticalSpeed = 0;
+			}
+
+
+			// Esquerda
+			if (Input.GetKey ("left")) {
+
+				if (horizontalSpeed > -horRotationLimit){
+					horizontalSpeed -= amountRotation * Time.deltaTime;
+				} else {
+					horizontalSpeed = -horRotationLimit;
+				}
+
+				transform.Rotate (0, horizontalSpeed, 0);
+			}
+
+
+			// Direita
+			if (Input.GetKey ("right")) {
+
+				if (horizontalSpeed < horRotationLimit){
+					horizontalSpeed += amountRotation * Time.deltaTime;
+				} else {
+					horizontalSpeed = horRotationLimit;
+				}
+
+				transform.Rotate (0, horizontalSpeed, 0);
+			}
+
+			// Resetando a velocidade de rotação horizontal
+			if (Input.GetKeyUp ("left") || Input.GetKeyUp ("right")) {
+				horizontalSpeed = 0;
+			}
+
+
+		}
+	}
+
+}
---
+++ Assets/scripts/Spaceship/SpaceshipZedRotation.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipZedRotation : MonoBehaviour {
+
+	// PS: Não há qualquer tutorial decente na internet sobre movimentação de aviões.
+	// PS2: Tomara que eu nunca mais pegue controles de naves 3D.
+
+	public bool isLocal = true;
+
+	public float timeLimit = 0.4f;		// A nave só pode girar no eixo z por no máximo 0.4 segundos
+	float rotationTimeLeft = 0.0f;		// Quanto tempo a nave está girando para a esquera
+	float rotationTimeRight = 0.0f;		// Quanto tempo a nave está girando para a direita
+	bool releasedLeft = false;			// Se true, o jogador soltou o botão de seta para a esquerda
+	bool releasedRight = false;			// Se true, o jogador soltou o botão de seta para a direita
+
+	public float rotationAmount = 105.0f;		// Velocidade de rotação no eixo z
+
+	void LateUpdate()
+	{
+		if (isLocal) {					// Matheus pediu pra colocar isso
+			
+			if (Input.GetKey ("left")) {
+
+				if (rotationTimeLeft < timeLimit && releasedRight == false) {		
+					releasedLeft = false;											// O jogador só irá rotacionar para a
+					transform.Rotate (0, 0, rotationAmount * Time.deltaTime);		// esquerda se ele não acabou de soltar
+					rotationTimeLeft += (1 - rotationTimeRight) * Time.deltaTime;	// o botão de seta pra direita. É
+																					// necessário para corrigir um bug
+					if (rotationTimeLeft > timeLimit) {
+						rotationTimeLeft = timeLimit;
+					}
+
+				}
+
+			}
+
+			if (Input.GetKey ("right")) {
+
+				if (rotationTimeRight < timeLimit && releasedLeft == false) {		
+					releasedRight = false;											// O jogador só irá rotacionar para a
+					transform.Rotate (0, 0, -rotationAmount * Time.deltaTime);		// direita se ele não acabou de soltar
+					rotationTimeRight += (1 - rotationTimeLeft) * Time.deltaTime;	// o botão de seta pra esquerda. É
+																					// necessário para corrigir um bug
+					if (rotationTimeRight > timeLimit) {
+						rotationTimeRight = timeLimit;
+					}
+				}
+
+			}
+
+
+			if (Input.GetKeyUp ("left")) {		// GetKeyUp só ocorre no frame em que a tecla é solta.
+				releasedLeft = true;			// No nosso caso, precisamos de um retorno contínuo, e
+			}									// as variáveis releasedLeft e releasedRight dão conta
+
+			if (Input.GetKeyUp ("right")) {
+				releasedRight = true;
+			}
+			//
+
+			if (releasedLeft) {
+
+				if (rotationTimeLeft > 0) {											// Ao soltar o botão de seta para a
+					transform.Rotate (0, 0, -rotationAmount * Time.deltaTime);		// esquerda, a nave irá desfazer sua
+					rotationTimeLeft -= 1 * Time.deltaTime;							// rotação durante um certo tempo, que é
+				}																	// definido pela variável rotationTimeLeft
+
+				if (rotationTimeLeft <= 0) {
+					rotationTimeLeft = 0;
+					releasedLeft = false;
+				}
+
+			}
+
+			if (releasedRight) {
+
+				if (rotationTimeRight > 0) {										// Ao soltar o botão de seta para a
+					transform.Rotate (0, 0, rotationAmount * Time.deltaTime);		// direita, a nave irá desfazer sua
+					rotationTimeRight -= 1 * Time.deltaTime;						// rotação durante um certo tempo, que é
+				}																	// definido pela variável rotationTimeRight
+
+				if (rotationTimeRight <= 0) {
+					rotationTimeRight = 0;
+					releasedRight = false;
+				}
+
+			}
+
+			// Debug.Log (rotationTimeLeft + " - " + rotationTimeRight);			// Confirmando o tempo de rotação restante
+
+		}
+	}
+}
